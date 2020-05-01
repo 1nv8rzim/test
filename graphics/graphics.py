3c85e5f12cafe584fd8bc1618f27e26ded6fb586
1nv8rzim <msf9542@g.rit.edu>
2020-04-30 20:57:00 -0400

    testing with changing gui with the gui itself

---
+++ graphics/graphics.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+
+class test_gui:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("test")
+        self.text = tk.StringVar()
+        self.temp = 1
+
+        self.text.set(str(self.temp))
+
+        self.labal = tk.Label(self.master, textvariable=self.text)
+        self.labal.pack()
+
+        self.button = tk.Button(self.master, text="Button", command=self.change_text)
+        self.button.pack()
+
+        self.close = tk.Button(self.master, text="Close", command=self.master.quit)
+        self.close.pack()
+
+        self.master.mainloop()
+
+    def change_text(self):
+        self.temp += 1
+        self.text.set(str(self.temp))
+
+
+root = tk.Tk()
+gui = test_gui(root)
