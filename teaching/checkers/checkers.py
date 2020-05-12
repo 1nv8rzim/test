ffd2749cf4b0d27fcc0b089d9f7ad63e17df5a5e
Maxwell <msf9542@g.rit.edu>
2020-05-11 22:09:16 -0400

    implemented gui

--- teaching/checkers/checkers.py
+++ teaching/checkers/checkers.py
@@ -1,3 +1,6 @@
+from tkinter import *
+
+
 class Checker:
     def __init__(self, color):
         self.color = color
@@ -25,8 +28,16 @@ class Checker:
         if self.color == self.color.upper():
             coordinates = [(self.row + 1, self.col + 1), (self.row + 1, self.color - 1),
                            (self.row - 1, self.col + 1), (self.row - 1, self.col - 1)]
+        elif self.color == 'w':
+            coordinates = [(self.row + 1, self.col + 1),
+                           (self.row + 1, self.color - 1)]
+        else:
+            coordinates = [(self.row - 1, self.col + 1),
+                           (self.row - 1, self.color - 1)]
+
         coordinates = [
             coordinate for coordinate in coordinates if Checker.isValidCoordinate(coordinate)]
+
         return coordinates
 
     @staticmethod
@@ -51,5 +62,31 @@ class Board:
     def __str__(self):
         return str(self.board)
 
+    def moves(self, row, col):
+        coordinates = self.board[row][col].moves()
+        coordinates = [
+            coordinate for coordinate in coordinates if self.board[coordinate[0]][coordinate[1]] == 0]
 
-print(Board())
+        return coordinates
+
+
+class GUI:
+    def __init__(self):
+        root = Tk()
+        root.grid()
+
+        board = Board()
+
+        buttons = []
+        for row in range(8):
+            button_row = []
+            for col in range(8):
+                button_row.append(
+                    Button(root, text=str(board.board[row][col])))
+                button_row[col].grid(row=row, column=col)
+            buttons.append(button_row)
+
+        root.mainloop()
+
+
+gui = GUI()
