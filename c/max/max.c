4530b4e0129f7323a17e09ddaf6fe9c90edeb5b5
Maxwell <msf9542@g.rit.edu>
2020-06-15 21:28:23 -0400

    minor changes

--- c/max/max.c
+++ c/max/max.c
@@ -5,11 +5,11 @@ int max(int num1, int num2)
 {
     if (num1 > num2)
     {
-        printf("%d", num1);
+        printf("%d\n", num1);
     }
     else
     {
-        printf("%d", num2);
+        printf("%d\n", num2);
     }
     return 0;
 }
