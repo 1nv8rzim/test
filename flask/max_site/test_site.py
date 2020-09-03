3ecfd2d90b11c83fab83d47b6e1ad3ee84a1be9b
Maxwell <msf9542@g.rit.edu>
2020-09-03 19:35:52 -0400

    basci changes

--- flask/max_site/test_site.py
+++ flask/max_site/test_site.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
